dd5c0beff359c4fef2d6df1fe77d709c1877abd3
Harvey Rogers <harveyr@gmail.com>
2024-07-22 22:02:49 -0700

    theirs vs. ours working

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 def main():
     st.title("Emily's thing")
-    emily_file = st.file_uploader("Emily's spreadsheet")
+    emily_file = st.file_uploader("Our spreadsheet (Emily's)")
     theirs_file = st.file_uploader("Their spreadsheet")
 
     if emily_file is not None:
@@ -27,11 +27,74 @@ def main():
         if theirs_file is not None:
             theirs_df = pd.read_excel(theirs_file)
 
-            handle(ours=ours_df, theirs=theirs_df)
+            # handle_ours_vs_theirs(ours=ours_df, theirs=theirs_df)
+            handle_theirs_vs_ours(ours=ours_df, theirs=theirs_df)
 
 
-def handle(ours: pd.DataFrame, theirs: pd.DataFrame):
-    missing_theirs = pd.DataFrame(columns=["ID", "Name", "Course", "Title"])
+def handle_theirs_vs_ours(ours: pd.DataFrame, theirs: pd.DataFrame):
+    missing = pd.DataFrame(
+        columns=["ID", "Name", "Course Prefix", "Course Number", "Title"]
+    )
+
+    for i, row in theirs.iterrows():
+        student_id = row["ID"]
+        student_name = row["Name"]
+        course_id = row["COURSE"]
+        course_title = row["TITLE"]
+
+        if not isinstance(student_id, str):
+            if not isinstance(course_id, str):
+                # This is an empty row
+                continue
+
+            raise ValueError(
+                f"Found a non-string student ID in theirs: {student_id} "
+                f"({student_name})"
+            )
+
+        first, last = [s.strip() for s in student_name.split(",")]
+        course_id: str = course_id.strip()
+        course_prefix = course_id[:4]
+        course_suffix = course_id[4:]
+
+        matched = False
+
+        for _, c_row in ours.iterrows():
+            if student_id != c_row["student_id"] and (
+                first != c_row["first"] and last != c_row["last"]
+            ):
+                continue
+
+            if course_id != c_row["prefix"].strip() + str(c_row["suffix"]).strip():
+                continue
+
+            matched = True
+            logger.info("Match: %s, %s", student_name, course_id)
+            break
+
+        # filtered = ours[
+        #     (ours["student_id"].str == student_id)
+        #     & (ours["prefix"].str.strip() == course_prefix)
+        #     & (ours["suffix"].str.strip() == course_suffix)
+        # ]
+
+        # TODO: if no match, try first last
+
+        if not matched:
+            missing.loc[i] = [
+                student_id,
+                student_name,
+                course_prefix,
+                course_suffix,
+                course_title,
+            ]
+
+    st.header("Registrations in their sheet missing from ours")
+    st.table(missing)
+
+
+def handle_ours_vs_theirs(ours: pd.DataFrame, theirs: pd.DataFrame):
+    missing = pd.DataFrame(columns=["ID", "Name", "Course", "Title"])
 
     for i, row in ours.iterrows():
         student_id: Optional[str] = row["student_id"]
@@ -58,7 +121,7 @@ def handle(ours: pd.DataFrame, theirs: pd.DataFrame):
             msg = "No match: {} - {} - {}".format(student_name, course_id, course_title)
             logger.info(msg)
             # st.write(msg)
-            missing_theirs.loc[i] = [
+            missing.loc[i] = [
                 student_id or "[not found]",
                 student_name,
                 course_id,
@@ -69,8 +132,8 @@ def handle(ours: pd.DataFrame, theirs: pd.DataFrame):
                 "Match: {} - {} - {}".format(student_name, course_id, course_title)
             )
 
-    st.header("Registrations missing from their sheet")
-    st.table(missing_theirs)
+    st.header("Registrations in our sheet missing from theirs")
+    st.table(missing)
 
 
 if __name__ == "__main__":
